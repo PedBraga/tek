6c796021bfbd7d82a6571e7d0908fa977d094723
Pedro <49527202+PedBraga@users.noreply.github.com>
2022-02-25 15:26:14 -0500

    update navs

--- js/script.js
+++ js/script.js
@@ -52,3 +52,21 @@ $(function() {
 
 
 // containerchoise section
+
+
+
+// Products section
+const activeImage = document.querySelector(".product-image .active");
+const productImages = document.querySelectorAll(".image-list img");
+const navItem = document.querySelector('a.toggle-nav');
+
+function changeImage(e) {
+  activeImage.src = e.target.src;
+}
+
+function toggleNavigation(){
+  this.nextElementSibling.classList.toggle('active');
+}
+
+productImages.forEach(image => image.addEventListener("click", changeImage));
+navItem.addEventListener('click', toggleNavigation);
